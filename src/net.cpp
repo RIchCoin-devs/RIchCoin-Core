593621a49c6d489382052ed9524282054061d94c
HTY2003 <randumbperson2003@gmail.com>
2018-05-06 01:01:26 +0800

    DNS Seeds removed

--- src/net.cpp
+++ src/net.cpp
@@ -1151,7 +1151,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = { //TODO: change
-    {"188.226.178.216", "188.226.155.37"},
+    {NULL, NULL},
 };
 
 void ThreadDNSAddressSeed(void* parg)
