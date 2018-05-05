cfd34df93df54b2953cb44ef3bedfb4abffcae4e
Heng Teng Yi <randumbperson2003@gmail.com>
2018-05-06 00:55:50 +0800

    Checkpoints disabled for pre-genesis

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -71,14 +71,16 @@ namespace Checkpoints
 
         MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
         if (i == checkpoints.end()) return true;
-        return hash == i->second;
+        // return hash == i->second;
+        return true;
     }
 
     int GetTotalBlocksEstimate()
     {
         MapCheckpoints& checkpoints = (fTestNet ? mapCheckpointsTestnet : mapCheckpoints);
 
-        return checkpoints.rbegin()->first;
+        // return checkpoints.rbegin()->first;
+        return 0;
     }
 
     CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
@@ -90,7 +92,8 @@ namespace Checkpoints
             const uint256& hash = i.second;
             std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
             if (t != mapBlockIndex.end())
-                return t->second;
+                // return t->second;
+                return NULL;
         }
         return NULL;
     }
