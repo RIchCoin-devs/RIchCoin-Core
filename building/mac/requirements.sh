2564a4318a52d5a199dead5439d3859acc83adfb
Heng Teng Yi <randumbperson2003@gmail.com>
2018-07-04 01:54:51 +0800

    Update requirements.sh

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,6 +1,6 @@
 brew uninstall qt5
-brew install boost@1.57
+brew install boost@1.60
 brew install qt@5.5 protobuf miniupnpc openssl qrencode berkeley-db4 pkg-config
 brew install libevent librsvg
 brew link --force qt@5.5
-brew link --force boost@1.57
+brew link --force boost@1.60
